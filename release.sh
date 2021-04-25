cda549186bd595d0b39688a999736a5966911818
刘天青 <liutianqing@dfsoft.com.cn>
2021-04-25 14:41:05 +0800

    release

--- release.sh
+++ release.sh
@@ -1,9 +1,21 @@
 #!/bin/bash
 
+# 选择版本
 echo -e "Please input type of version:"
 echo -e "1. patch\n2. minor"
 read -p "please choose 1/2: " -t 30 num
 
+# 打包
+npm run lib
+
+# 发版
+npm publish
+
+# 提交代码
+git add -A
+git commit -m "chore: library"
+
+# 升级版本
 case $num in
   "1")
     npm version patch
@@ -15,3 +27,6 @@ case $num in
     echo "your choose is error"
     ;;
 esac
+
+# 推送代码
+git push origin develop
